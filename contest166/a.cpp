4d7dea0d09f8b8cc624947fcc8f295cb581d671c
lorderikstark0 <sagnik.chatterjee61@gmail.com>
2020-06-08 09:09:29 +0530

    added solved questions

---
+++ contest166/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	string s;
+	cin >> s;
+
+	if(s=="ABC"){
+		cout << "ARC"<<"\n";
+	}
+	else{
+		cout <<"ARC" <<"\n";
+	}
+
+	return 0;
+}
